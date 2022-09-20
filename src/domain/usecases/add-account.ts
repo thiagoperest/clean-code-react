e6c7aac88c9d77c334157887dd39c3cb01af8e9b
thiagoperest <thiago_teodoroperes07@hotmail.com>
2022-09-20 18:50:16 -0300

    feat: add LoadSurveyList usecase

--- src/domain/usecases/add-account.ts
+++ src/domain/usecases/add-account.ts
@@ -1,11 +1,11 @@
-import { AccountModel } from '@/domain/models/account-model'
+import { AccountModel } from '@/domain/models'
 
 export type AddAccountParams = {
   name: string
   email: string
   password: string
   passwordConfirmation: string
-};
+}
 
 export interface AddAccount {
   add: (params: AddAccountParams) => Promise<AccountModel>
