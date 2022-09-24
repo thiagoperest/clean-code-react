98ebc16b6812c11cd03a6bb2c04193bd4ac66d9c
thiagoperest <thiago_teodoroperes07@hotmail.com>
2022-09-24 09:20:47 -0300

    feat: add initial survey-list view

---
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import Styles from './survey-list-styles.scss'
+import { Logo, Footer } from '@/presentation/components'
+
+const SurveyList: React.FC = () => {
+  return (
+    <div className={Styles.surveyListWrap}>
+      <header className={Styles.headerWrap}>
+        <div className={Styles.headerContent}>
+          <Logo />
+          <div className={Styles.logoutWrap}>
+            <span>Thiago</span>
+            <a href="#">Sair</a>
+          </div>
+        </div>
+      </header>
+      <div className={Styles.contentWrap}>
+        <h2>Enquetes</h2>
+        <ul>
+          <li>
+            <div className={Styles.surveyContent}>
+              <time>
+                <span className={Styles.day}>24</span>
+                <span className={Styles.month}>09</span>
+                <span className={Styles.year}>2022</span>
+              </time>
+              <p>Qual é o seu framework web favorito?</p>
+            </div>
+            <footer>Ver Resultado</footer>
+          </li>
+        </ul>
+      </div>
+      <Footer />
+    </div>
+  )
+}
+
+export default SurveyList
