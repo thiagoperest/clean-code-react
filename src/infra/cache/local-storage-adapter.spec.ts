62f18d0086f90527da34267b70b57828168de58b
thiagoperest <thiago_teodoroperes07@hotmail.com>
2022-09-24 14:29:41 -0300

    refactor: call async function method

--- src/infra/cache/local-storage-adapter.spec.ts
+++ src/infra/cache/local-storage-adapter.spec.ts
@@ -1,6 +1,6 @@
 import faker from 'faker'
 import 'jest-localstorage-mock'
-import { LocalStorageAdapter } from './local-storage-adapter';
+import { LocalStorageAdapter } from './local-storage-adapter'
 
 const makeSut = (): LocalStorageAdapter => new LocalStorageAdapter()
 
@@ -13,7 +13,7 @@ describe('LocalStorageAdapter', () => {
     const sut = makeSut()
     const key = faker.database.column()
     const value = faker.random.word()
-    await sut.set(key, value)
+    sut.set(key, value)
     expect(localStorage.setItem).toHaveBeenCalledWith(key, value)
   })
 })
