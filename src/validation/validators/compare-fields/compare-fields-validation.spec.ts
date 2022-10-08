ba55c92410ab2b1d2a31d338d98b515ecc5bca8e
thiagoperest <thiago_teodoroperes07@hotmail.com>
2022-10-08 09:46:53 -0300

    fix: change test to ensure values are different

---
+++ src/validation/validators/compare-fields/compare-fields-validation.spec.ts
@@ -0,0 +1,30 @@
+import { CompareFieldsValidation } from '@/validation/validators'
+import { InvalidFieldError } from '@/validation/errors'
+import faker from 'faker'
+
+const makeSut = (field: string, fieldToCompare: string): CompareFieldsValidation => new CompareFieldsValidation(field, fieldToCompare)
+
+describe('CompareFieldsValidation', () => {
+  test('Should return error if compare is invalid', () => {
+    const field = faker.database.column()
+    const fieldToCompare = faker.database.column()
+    const sut = makeSut(field, fieldToCompare)
+    const error = sut.validate({
+      [field]: 'any_value',
+      [fieldToCompare]: 'other_value'
+    })
+    expect(error).toEqual(new InvalidFieldError())
+  })
+
+  test('Should return falsy if compare is valid', () => {
+    const field = faker.database.column()
+    const fieldToCompare = faker.database.column()
+    const value = faker.random.word()
+    const sut = makeSut(field, fieldToCompare)
+    const error = sut.validate({
+      [field]: value,
+      [fieldToCompare]: value
+    })
+    expect(error).toBeFalsy()
+  })
+})
--- src/validation/validators/min-length/min-length-validation.spec.ts
+++ src/validation/validators/min-length/min-length-validation.spec.ts
@@ -1,5 +1,5 @@
 import { InvalidFieldError } from '@/validation/errors'
-import { MinLengthValidation } from './min-length-validation'
+import { MinLengthValidation } from '@/validation/validators'
 import faker from 'faker'
 
 const makeSut = (field: string): MinLengthValidation => new MinLengthValidation(field, 5)
@@ -20,8 +20,8 @@ describe('MinLengthValidation', () => {
   })
 
   test('Should return falsy if field does not exists in schema', () => {
-    const sut = makeSut(faker.database.column())
-    const error = sut.validate({ [faker.database.column()]: faker.random.alphaNumeric(5) })
+    const sut = makeSut('any_field')
+    const error = sut.validate({ invalidField: faker.random.alphaNumeric(5) })
     expect(error).toBeFalsy()
   })
 })
