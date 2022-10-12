555734a314db897814d675fc03817ff7ceb751ff
thiagoperest <thiago_teodoroperes07@hotmail.com>
2022-10-12 10:05:37 -0300

    feat: ensure SurveyResult calls LoadSurveyResult

--- src/presentation/pages/survey-result/survey-result.tsx
+++ src/presentation/pages/survey-result/survey-result.tsx
@@ -1,15 +1,25 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Styles from './survey-result-styles.scss'
 import { Header, Footer, Loading, Calendar, Error } from '@/presentation/components'
 import FlipMove from 'react-flip-move'
 import { LoadSurveyResult } from '@/domain/usecases'
 
-const SurveyResult: React.FC = () => {
+type Props = {
+  loadSurveyResult: LoadSurveyResult
+}
+
+const SurveyResult: React.FC<Props> = ({ loadSurveyResult }: Props) => {
   const [state] = useState({
     isLoading: false,
     error: '',
     surveyResult: null as LoadSurveyResult.Model
   })
+
+  useEffect(() => {
+    loadSurveyResult.load()
+      .then()
+      .catch()
+  }, [])
 
   return (
     <div className={Styles.surveyResultWrap}>
