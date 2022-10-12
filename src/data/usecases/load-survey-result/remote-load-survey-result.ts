93bdf9d69d4e6f58dde8801cd75ec75017ad0813
thiagoperest <thiago_teodoroperes07@hotmail.com>
2022-10-12 16:30:35 -0300

    feat: ensure RemoteSaveSurveyResult calls HttpClient with correct values

--- src/data/usecases/load-survey-result/remote-load-survey-result.ts
+++ src/data/usecases/load-survey-result/remote-load-survey-result.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpStatusCode } from '@/data/protocols/http'
 import { AccessDeniedError, UnexpectedError } from '@/domain/errors'
 import { LoadSurveyResult } from '@/domain/usecases'
+import { RemoteSurveyResultModel } from '@/data/models'
 
 export class RemoteLoadSurveyResult implements LoadSurveyResult {
   constructor (
@@ -23,15 +24,5 @@ export class RemoteLoadSurveyResult implements LoadSurveyResult {
 }
 
 export namespace RemoteLoadSurveyResult {
-  export type Model = {
-    question: string
-    date: string
-    answers: Array<{
-      image?: string
-      answer: string
-      count: number
-      percent: number
-      isCurrentAccountAnswer: boolean
-    }>
-  }
+  export type Model = RemoteSurveyResultModel
 }
